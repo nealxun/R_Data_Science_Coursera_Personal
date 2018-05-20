e1083bec3d1ef6b15718e867ad36cfa24c47c8e8
Neal <nealxun@gmail.com>
2018-05-20 13:34:24 -0700

    Add files via upload

---
+++ packages_list.R
@@ -0,0 +1,38 @@
+# a list of most frequenty used R packages
+# test environment
+rm(list = ls())
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
+
+# update R
+install.packages("installr")
+library(installr)
+updateR()
+# check version
+sessionInfo()
+# check r version
+R.Version()
+# check specific package verison
+packageVersion("shiny")
+
+# package list
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("caret")
+install.packages("AppliedPredictiveModeling")
+install.packages("Hmisc")
+install.packages("ElemStatLearn")
+install.packages("gbm")
+install.packages("forecast")
+install.packages("elasticnet")
+install.packages("lubridate")
+install.packages("e1071")
+install.packages("shiny")
+install.packages("plotly")
+install.packages("leaflet")
+install.pakcages("reshape")
+
+
+
